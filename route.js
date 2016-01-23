66ab991ccd9fbad24294e32e0531133e6d33e1ac
Tyler Cole <tyler@iamtyler.com>
2016-01-22 20:35:58 -0800

    first pass at a simple http router

---
+++ route.js
@@ -0,0 +1,222 @@
+/****************************************************************************
+*
+*   route.js
+*   reddum
+*
+***/
+
+"use strict";
+
+
+/****************************************************************************
+*
+*   Handler
+*
+***/
+
+function Handler (method, path, chain, params) {
+    this.method = method;
+    this.path = path;
+    this.chain = chain;
+    this.params = params;
+    this.args = undefined;
+}
+
+//===========================================================================
+Handler.prototype.execute = function (req, res) {
+    var params = {}, il = this.params.length;
+    for (let i = 0; i < il; ++i) {
+        params[this.params[i]] = this.args[i];
+    }
+
+    var state = {
+            req : req,
+            res : res,
+            params : params,
+        },
+        chain = this.chain,
+        next = function () {
+            var fn = chain.shift();
+            if (fn) {
+                fn(state, next);
+            }
+        };
+
+    next();
+}
+
+//===========================================================================
+Handler.prototype.bind = function (args) {
+    var handler = new Handler(
+        this.method,
+        this.path,
+        this.chain,
+        this.params
+    );
+    handler.args = args;
+    return handler;
+}
+
+
+/****************************************************************************
+*
+*   Node
+*
+***/
+
+function Node () {
+    this.methods = {};
+    this.values = {};
+    this.variable = undefined;
+}
+
+//===========================================================================
+Node.prototype.register = function (method, path, parts, chain, params) {
+    if (parts.length === 0) {
+        this.methods[method] = new Handler(method, path, chain, params);
+        return true;
+    }
+
+    var part = parts.shift(),
+        node;
+
+    if (part.startsWith(":")) {
+        node = this.variable || (this.variable = new Node());
+        params.push(part.substring(1));
+    }
+    else {
+        node = this.values[part] || (this.values[part] = new Node());
+    }
+
+    return node.register(method, path, parts, chain, params);
+}
+
+//===========================================================================
+Node.prototype.match = function (method, parts, args) {
+    if (parts.length === 0) {
+        var handler = this.methods[method];
+        return handler && handler.bind(args);
+    }
+
+    var part = parts.shift(),
+        node = this.values[part];
+
+    if (!node && this.variable) {
+        node = this.variable;
+        args.push(part);
+    }
+
+    if (!node) {
+        return undefined;
+    }
+
+    return node.match(method, parts, args);
+}
+
+
+/****************************************************************************
+*
+*   Router
+*
+***/
+
+function Router () {
+    this.root = new Node();
+}
+
+//===========================================================================
+Router.prototype.add = function (method, path) {
+    var parts = splitPath(path);
+    if (parts === null) {
+        return false;
+    }
+
+    var chain = Array.prototype.slice.call(arguments, 2);
+
+    return this.root.register(method, path, parts, flattenChain(chain), []);
+}
+
+//===========================================================================
+Router.prototype.get = function (path) {
+    this.add("GET", path, Array.prototype.slice.call(arguments, 1));
+}
+
+//===========================================================================
+Router.prototype.put = function (path) {
+    this.add("PUT", path, Array.prototype.slice.call(arguments, 1));
+}
+
+//===========================================================================
+Router.prototype.post = function (path) {
+    this.add("POST", path, Array.prototype.slice.call(arguments, 1));
+}
+
+//===========================================================================
+Router.prototype.delete = function (path) {
+    this.add("DELETE", path, Array.prototype.slice.call(arguments, 1));
+}
+
+//===========================================================================
+Router.prototype.route = function (method, path) {
+    var parts = splitPath(path);
+    if (parts === null) {
+        return null;
+    }
+
+    return this.root.match(method, parts, []);
+}
+
+
+/****************************************************************************
+*
+*   Functions
+*
+***/
+
+//===========================================================================
+function splitPath (path) {
+    if (path === "/") {
+        return [];
+    }
+
+    if (path.startsWith("/")) {
+        path = path.substring(1);
+    }
+    else {
+        return null;
+    }
+
+    if (path.endsWith("/")) {
+        path = path.substring(0, path.length - 1);
+    }
+
+    return path.split("/");
+}
+
+//===========================================================================
+function flattenChain (chain) {
+    if (!(chain instanceof Array)) {
+        return [ chain ];
+    }
+
+    var a = [];
+    for (let fn of chain) {
+        if (fn instanceof Array) {
+            Array.prototype.push.apply(a, flattenChain(fn));
+        }
+        else {
+            a.push(fn);
+        }
+    }
+
+    return a;
+}
+
+
+/****************************************************************************
+*
+*   Exports
+*
+***/
+
+exports.Router = Router;
---
+++ test.js
@@ -0,0 +1,53 @@
+"use strict";
+
+var r = require("rethinkdb"),
+    route = require("./route");
+
+var router = new route.Router();
+
+router.get("/", () => console.log("root handler"));
+router.get("/test", () => console.log("test handler"));
+router.get("/test/two", () => console.log("test two handler"));
+
+router.get("/test/:var/echo",
+    (state) => console.log("test echo handler:", state.params.var)
+);
+
+router.get("/chain",
+    (state, next) => { console.log("chain 1"); next(); },
+    (state, next) => { console.log("chain 2"); next(); },
+    [
+        (state, next) => { console.log("chain 3"); next(); },
+        (state, next) => { console.log("chain 4"); next(); },
+    ],
+    (state, next) => { console.log("chain 5"); next(); }
+);
+
+router.get("/early",
+    (state, next) => console.log("early 1"),
+    () => console.log("early 2")
+);
+
+function execute (method, path) {
+    console.log("execute:", method, path);
+    var handler = router.route(method, path);
+    if (handler) {
+        console.log("FOUND:", handler.method, handler.path);
+        handler.execute();
+    }
+    else {
+        console.log("NOT FOUND");
+    }
+    console.log();
+}
+
+execute("GET", "/");
+execute("GET", "/test");
+execute("GET", "/nothing");
+execute("GET", "/test/two");
+execute("GET", "/test/yep/echo");
+execute("GET", "/test/yep");
+execute("GET", "/chain");
+execute("GET", "/early");
+
+
